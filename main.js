fd9d41ae805af8a5e6d89fe125c27ff14ffcdd8a
Martin Grossmann <mhgrossmann@yahoo.com>
2016-08-18 16:22:00 -0500

    added readme with information on the application

---
+++ main.js
@@ -0,0 +1,21 @@
+/*
+An application that uses AJAX to store information on my favorite beers.
+The following information will be stored:
+1 Beer Name
+2 Type of Beer
+3 Brewery Name
+4 Location of Brewery - City and State
+5 Where I had the beer
+
+Phase 2 will allow you to upload a photo of the bottle/glass to the application for each beer.
+-also allow for rating the beer
+
+Use Tiny Pizza Server for AJAX requests in this application:
+url: 'http://small-tiyfe.herokuapp.com/collections/mgrossmann',
+
+*/
+
+
+
+
+
